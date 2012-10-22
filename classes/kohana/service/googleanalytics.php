39f8f7a465c050b48b788c21b4741fa1d1134a92
Ivan Kerin <ivan@openbuildings.com>
2012-10-22 15:42:25 +0300

    add test library that integrates to Kissmetrics and GA

--- classes/kohana/service/googleanalytics.php
+++ classes/kohana/service/googleanalytics.php
@@ -11,7 +11,18 @@
  */
 abstract class Kohana_Service_Googleanalytics extends Service implements Service_Type_Javascript
 {
+	const SCOPE_VISITOR = 1;
+	const SCOPE_SESSION = 2;
+	const SCOPE_PAGE = 3;
+
 	protected $_access_token;
+	protected $_custom_vars = array();
+	
+	public function set_custom_var($index, $name, $value, $opt_scope = Service_Googleanalytics::SCOPE_PAGE)
+	{
+		$this->_custom_vars[] = array($index, $name, $value, $opt_scope);
+		return $this;
+	}
 
 	public function init()
 	{
@@ -24,14 +35,21 @@ abstract class Kohana_Service_Googleanalytics extends Service implements Service
 	 */
 	public function code()
 	{
-		if ( ! $this->initialized() OR ! $api_key = $this->config('api-key'))
+		if ( ! $this->initialized())
 			return NULL;
+
+		$custom_vars = '';
+		foreach ($this->_custom_vars as $var) 
+		{
+			$custom_vars .= "_gaq.push(['_setCustomVar', {$var[0]}, {$var[1]}, {$var[2]}, {$var[3]}]);\n";
+		}
 
 		return <<<ANALYTICS
 <script type="text/javascript">
 	var _gaq = _gaq || [];
 	_gaq.push(['_setAccount', '{$api_key}']);
 	_gaq.push(['_trackPageview']);
+	{$custom_vars}
 	(function() {
 	  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 	  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
